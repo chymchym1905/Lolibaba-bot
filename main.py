fb32cb59eae27a145a99194edeb981e1cb65bbd0
chymchym1905 <kien.dauoofman@hcmut.edu.vn>
2025-02-05 15:45:17 +0700

    update delay

--- main.py
+++ main.py
@@ -218,7 +218,7 @@ class MyClient(discord.Client):
 
                 try:
                     msg: Message = await self.wait_for(
-                        "message", timeout=random.uniform(0.0, 1.0), check=check_next
+                        "message", timeout=random.uniform(1.5,1.6), check=check_next
                     )
                     if is_positive_number(msg.content):
                         raise ValueError(f"{msg.author} counted: {msg.content}")
