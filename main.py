6f6fbb56b215364f3366dabe5288d0555fc0bbab
chymchym1905 <chymchym1905@gmail.com>
2025-02-13 22:06:00 +0700

    disable counting

--- main.py
+++ main.py
@@ -175,61 +175,61 @@ class MyClient(discord.Client):
         if message.author == self.user:
             return  # Ignore bot's own messages
 
-        if self.check_ruined(message):
-            await message.channel.typing()
-            await message.channel.send("1")
-            return 
-        if message.guild and message.channel.id in COUNT_CHANNELS:
-            if is_positive_number(message.content):  # Count from this message
+        # if self.check_ruined(message):
+        #     await message.channel.typing()
+        #     await message.channel.send("1")
+        #     return 
+        # if message.guild and message.channel.id in COUNT_CHANNELS:
+        #     if is_positive_number(message.content):  # Count from this message
 
-                def check(reaction, user):
-                    return str(user) == "counting#5250" and str(reaction.emoji) in [
-                        "\N{HUNDRED POINTS SYMBOL}",
-                        "\N{WHITE HEAVY CHECK MARK}",
-                        "\N{BALLOT BOX WITH CHECK}"
-                    ]
+        #         def check(reaction, user):
+        #             return str(user) == "counting#5250" and str(reaction.emoji) in [
+        #                 "\N{HUNDRED POINTS SYMBOL}",
+        #                 "\N{WHITE HEAVY CHECK MARK}",
+        #                 "\N{BALLOT BOX WITH CHECK}"
+        #             ]
 
-                def check_next(msg: Message):
-                    if msg.author == self.user:
-                        return False
-                    if msg.channel.id == message.channel.id:
-                        return is_positive_number(msg.content)
-                    return False
+        #         def check_next(msg: Message):
+        #             if msg.author == self.user:
+        #                 return False
+        #             if msg.channel.id == message.channel.id:
+        #                 return is_positive_number(msg.content)
+        #             return False
 
-                def check_valid(msg: Message):
-                    if self.check_ruined(msg):
-                        return True
-                    return False
+        #         def check_valid(msg: Message):
+        #             if self.check_ruined(msg):
+        #                 return True
+        #             return False
 
-                try:
-                    reaction, user = await self.wait_for(
-                        "reaction_add", timeout=10.0, check=check
-                    )
-                except Exception as e:
-                    return
+        #         try:
+        #             reaction, user = await self.wait_for(
+        #                 "reaction_add", timeout=10.0, check=check
+        #             )
+        #         except Exception as e:
+        #             return
 
-                # try:
-                #     msg: Message = await self.wait_for(
-                #         "message", timeout=1.0, check=check_valid
-                #     )
-                #     return
-                # except asyncio.TimeoutError:
-                #     pass
+        #         # try:
+        #         #     msg: Message = await self.wait_for(
+        #         #         "message", timeout=1.0, check=check_valid
+        #         #     )
+        #         #     return
+        #         # except asyncio.TimeoutError:
+        #         #     pass
 
-                # try:
-                #     msg: Message = await self.wait_for(
-                #         "message", timeout=random.uniform(0.0, 0.3), check=check_next
-                #     )
-                #     if is_positive_number(msg.content):
-                #         raise ValueError(f"{msg.author} counted: {msg.content}")
-                # except ValueError as e:
-                #     print(e)
-                #     return
-                # except asyncio.TimeoutError:
-                #     pass
+        #         # try:
+        #         #     msg: Message = await self.wait_for(
+        #         #         "message", timeout=random.uniform(0.0, 0.3), check=check_next
+        #         #     )
+        #         #     if is_positive_number(msg.content):
+        #         #         raise ValueError(f"{msg.author} counted: {msg.content}")
+        #         # except ValueError as e:
+        #         #     print(e)
+        #         #     return
+        #         # except asyncio.TimeoutError:
+        #         #     pass
 
-                # await message.channel.typing()
-                await message.channel.send(int(round(float(eval(message.content))) + 1))
+        #         # await message.channel.typing()
+        #         await message.channel.send(int(round(float(eval(message.content))) + 1))
             
 
         if message.guild and message.channel.id in AI_CHANNELS:
